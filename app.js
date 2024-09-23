d25a2e66e4ba746c779ce52ce79b14478f867b2f
Dolgion7 <odolgion222@gmail.com>
2024-09-23 12:12:36 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+// frontend/src/App.js
+import React from 'react';
+import FaceDetection from './FaceDetection';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Face Detection App</h1>
+            <FaceDetection />
+        </div>
+    );
+};
+
+export default App;
